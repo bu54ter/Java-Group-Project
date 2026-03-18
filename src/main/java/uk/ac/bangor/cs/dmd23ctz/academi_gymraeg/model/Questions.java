687c1adc295908824d5c937edc894f40590be447
buster <buster@localhost>
2026-03-18 21:06:19 +0000

    Populated model for remaining tables to complete questions and tests.

--- src/main/java/uk/ac/bangor/cs/dmd23ctz/academi_gymraeg/model/Questions.java
+++ src/main/java/uk/ac/bangor/cs/dmd23ctz/academi_gymraeg/model/Questions.java
@@ -1,5 +1,58 @@
 package uk.ac.bangor.cs.dmd23ctz.academi_gymraeg.model;
 
-public class Questions {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "ag_questions")
+public class Questions {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "question_id")
+	private Long questionId;
+	@Column(name = "test_id")
+	private Long testId;
+	@Column(name = "noun_id")
+	private Long nounId;
+	@Enumerated(EnumType.STRING)
+	@Column(name = "question_type", nullable = false)
+	private QuestionType questionType;
+
+	public Long getQuestionId() {
+		return questionId;
+	}
+
+	public void setQuestionId(Long questionId) {
+		this.questionId = questionId;
+	}
+
+	public Long getTestId() {
+		return testId;
+	}
+
+	public void setTestId(Long testId) {
+		this.testId = testId;
+	}
+
+	public Long getNounId() {
+		return nounId;
+	}
+
+	public void setNounId(Long nounId) {
+		this.nounId = nounId;
+	}
+
+	public QuestionType getQuestionType() {
+		return questionType;
+	}
+
+	public void setQuestionType(QuestionType questionType) {
+		this.questionType = questionType;
+	}
 }
--- src/main/java/uk/ac/bangor/cs/dmd23ctz/academi_gymraeg/model/Role.java
+++ src/main/java/uk/ac/bangor/cs/dmd23ctz/academi_gymraeg/model/Role.java
@@ -1,7 +1,5 @@
 package uk.ac.bangor.cs.dmd23ctz.academi_gymraeg.model;
 
 public enum Role {
-    ADMIN,
-    LECTURER,
-    STUDENT
+	ADMIN, LECTURER, STUDENT
 }
